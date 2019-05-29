65d1b3275e4ca0080b59c22f58b0b5c1d59e0098
Seán Murray <sean@murray.cc>
2019-05-29 18:28:58 +0000

    Update suite version. Bug fixes

---
+++ index.js
@@ -0,0 +1,80 @@
+/* jshint esversion: 6 */
+// Copyright (c) 2019 Seán D. Murray
+// SEE MIT LICENSE FILE
+const crypto = require('crypto');
+const isit = require('prose_isit');
+const array_util = require('prose_array');
+const string_util = require('prose_string');
+
+const HASH_GENERATOR = 'sha512';
+const HASH_ENCODING = 'hex';
+
+exports.copy = (obj, orderItems = false) => {
+	if (isit.aPrimitive(obj)) {
+		return obj;
+	}
+
+	if (isit.anArray(obj)) {
+		return array_util.copy(obj, orderItems);
+	}
+
+	let result = {};
+	let keys = Object.keys(obj);
+	if (orderItems) {
+		keys.sort();
+	}
+	keys.forEach((key) => {
+		let value = obj[key];
+		result[key] = exports.copy(value, orderItems);
+	});
+	return result;
+};
+
+exports.isEmpty = (obj) => {
+	if (isit.nil(obj)) {
+		return true;
+	}
+	if (isit.notObject(obj)) {
+		throw new Error('Tried to treat a non object as an object');
+	}
+	return (Object.keys(obj).length === 0) ? true : false;
+};
+
+exports.notEmpty = (obj) => {
+	return exports.isEmpty(obj) ? false : true;
+};
+
+exports.equal = (obj1, obj2, orderItems = true) => {
+	if (isit.notDefined(obj1) && isit.notDefined(obj1)) {
+		return true;
+	}
+	if (isit.nil(obj1) && isit.nil(obj1)) {
+		return true;
+	}
+	let hash1 = exports.toHash(obj1, orderItems);
+	let hash2 = exports.toHash(obj2, orderItems);
+	return hash1 === hash2 ? true : false;
+};
+
+exports.has = (obj, key) => {
+	if (isit.notObject(obj) || isit.nil(key)) {
+		return false;
+	}
+	if (isit.notNil(obj[key])) {
+		return true;
+	}
+	return false;
+};
+
+exports.notHave = (obj, key) => {
+	return exports.has(obj, key) ? false : true;
+};
+
+exports.toHash = (obj, orderItems = true) => {
+	if (isit.nil(obj)) {
+		return '0';
+	}
+	let deterministicCopy = exports.copy(obj, orderItems);
+	let stringToHash = string_util.toString(deterministicCopy);
+	return crypto.createHash(HASH_GENERATOR).update(stringToHash).digest(HASH_ENCODING);
+};
